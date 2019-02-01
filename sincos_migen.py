4bbc3f324bc49a94955dad49fcff49649435d240
DurandA <arnaud.durand@unifr.ch>
2019-02-01 23:49:49 +0100

    Pass Migen signals in parameters

--- sincos_migen.py
+++ sincos_migen.py
@@ -1,51 +1,32 @@
 from migen import *
-from itertools import starmap
-
-
-def bulk_assign(siglist, *signals):
-    return [a.eq(b) for a, b in zip(siglist, signals)]
 
 
 class HalfMultiplyAdd(Module):
-    def __init__(self):
-        self.a = Signal((16, True))
-        self.b = Signal((16, True))
-        self.c = Signal((16, True))
-        self.p = Signal((16, True))
+    def __init__(self, a, b, c, p):
         self.xx = xx = Signal(32)
-        self.comb += xx.eq(self.a * self.b)
-        self.comb += self.p.eq(xx[16:] + self.c)
-        self.inputs = (self.a, self.b, self.c)
-        self.outputs = (self.p,)
+        self.comb += xx.eq(a * b)
+        self.comb += p.eq(xx[16:] + c)
 
 
 class ISin(Module):
-    def __init__(self):
-        self.x = x = Signal((16, True))
-        self.s = s = Signal((16, True))
-
+    def __init__(self, x, s):
         y = Signal((16, True))
         cc = Signal((16, True))
 
         n = Signal(3)
         self.comb += n.eq(x[13:16] + Constant(1, 3))
         y = Cat(Constant(0, 2), x[:14])
-        
-        h0 = HalfMultiplyAdd()
-        hc = HalfMultiplyAdd()
-        hs = HalfMultiplyAdd()
-        h2 = HalfMultiplyAdd()
+
+        z = Signal((16, True))
+        sumc = Signal((16, True))
+        sums = Signal((16, True))
+        sum1 = Signal((16, True))
+
+        h0 = HalfMultiplyAdd(y, y, Constant(0), z)
+        hc = HalfMultiplyAdd(z, Constant(0x0fbd, (16, True)), Constant(-0x4ee9, (16, True)), sumc)
+        hs = HalfMultiplyAdd(z, Constant(0x04f8, (16, True)), Constant(-0x2953, (16, True)), sums)
+        h2 = HalfMultiplyAdd(z, sums, Constant(0x6487, (16, True)), sum1)
         self.submodules += [h0, hc, hs, h2]
-
-        z = h0.p
-        sumc = hc.p
-        sums = hs.p
-        sum1 = h2.p
-
-        self.comb += bulk_assign(h0.inputs, y, y, Constant(0))
-        self.comb += bulk_assign(hc.inputs, z, Constant(0x0fbd, (16, True)), Constant(-0x4ee9, (16, True)))
-        self.comb += bulk_assign(hs.inputs, z, Constant(0x04f8, (16, True)), Constant(-0x2953, (16, True)))
-        self.comb += bulk_assign(h2.inputs, z, sums, Constant(0x6487, (16, True)))
 
         t0 = Signal((16, True))
         t1 = Signal((16, True))
@@ -72,28 +53,26 @@ class ISin(Module):
 
 
 class ICos(Module):
-    def __init__(self):
-        self.x = Signal((16, True))
-        self.s = Signal((16, True))
-
-        isin = ISin()
+    def __init__(self, x, s):
+        isin = ISin(x + Constant(0x4000), s)
         self.submodules += isin
-        self.comb += isin.x.eq(self.x + Constant(0x4000))
-        self.comb += self.s.eq(isin.s)
 
 
 class ISinCosTest(Module):
     def __init__(self):
-        self.isin, self.icos = ISin(), ICos()
+        self.x = Signal((16, True))
+        self.iss = Signal((16, True))
+        self.ics = Signal((16, True))
+        self.isin, self.icos = ISin(self.x, self.iss), ICos(self.x, self.ics)
         self.submodules += [self.isin, self.icos]
 
 
 def testbench(dut):
     for i in range(65536):
-        yield dut.isin.x.eq(Constant(i, (16, True)))
-        yield dut.icos.x.eq(Constant(i, (16, True)))
-        print("{} {}".format((yield dut.isin.s), (yield dut.icos.s)))
+        yield dut.x.eq(Constant(i, (16, True)))
+        print("{} {}".format((yield dut.iss), (yield dut.ics)))
         yield
 
-dut = ISinCosTest()
-run_simulation(dut, testbench(dut), vcd_name="isin.vcd")
+if __name__ == "__main__":
+    dut = ISinCosTest()
+    run_simulation(dut, testbench(dut), vcd_name="isin.vcd")
